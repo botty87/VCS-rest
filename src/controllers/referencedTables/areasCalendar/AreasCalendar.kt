9d01949dc2fbe475f534036d2e20996c40c7a8d2
botty <bottichio.marco@gmail.com>
2020-10-16 15:54:52 +0200

    Working on login

--- src/controllers/referencedTables/areasCalendar/AreasCalendar.kt
+++ src/controllers/referencedTables/areasCalendar/AreasCalendar.kt
@@ -3,10 +3,11 @@ package controllers.referencedTables.areasCalendar
 import controllers.areas.Areas
 import controllers.retires.Retires
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 
 object AreasCalendar: IntIdTable() {
-    val area = reference("area", Areas).index()
-    val retire = reference("retire", Retires)
+    val area = reference("area", Areas, onDelete = ReferenceOption.CASCADE).index()
+    val retire = reference("retire", Retires, onDelete = ReferenceOption.CASCADE)
     val weekDay = byte("weekDay")
     override val primaryKey = PrimaryKey(area, retire, weekDay)
 }
